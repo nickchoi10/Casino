7a5192f470699920b8c8c20a66d1109dd4e158d2
Git-Yee <git-yee@ZipcodersMBP58.fios-router.home>
2022-07-11 11:50:23 -0400

    figuring out the rules of the game

---
+++ src/main/java/Blackjack.java
@@ -0,0 +1,35 @@
+public class Blackjack {
+
+    // fields:
+    // Deck
+    // Player's hand
+    // Dealer's hand
+
+    // BJ is usually a table of 2-7 players and uses one to 52-card deck.
+    // For the sake of my sanity, it will be dealer vs single player.
+    // Jack, Queen, King are all 10 points. Ace is one or 11 points.
+    // deal one card face up to all players, then deal second card to all players.
+    // dealer receive two card, one face up and one face down.
+    // Objective is to get 21 or be as close you can to 21 without going over.
+    // Beginning of game players place their bets.
+    // "Hit" or "Stand" without being busted (exceeding 21).
+    // After each player Hit or Stand, dealer's turn to hit or stand.
+    // Players can also Split, Double Down, Insurance (if dealer's up card is an ace, Surrender (give up half your bet)
+    // Sadly, our casino is not offering these options.
+    public void startGame{
+
+    }
+
+    public String hitOrStand{
+
+    }
+
+    public Deck cardFaceUp{
+
+    }
+
+    public Deck cardFaceDown{
+
+    }
+
+}
