d0b2d8d3c27479db67fd034597ae38520e144c39
kylehwelch <welch@udel.edu>
2022-07-16 09:58:55 -0400

    boulder parchment shears more updates

--- src/main/java/com/github/zipcodewilmington/casino/games/BoulderParchmentShears/BPSMain.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/BoulderParchmentShears/BPSMain.java
@@ -12,14 +12,14 @@ public class BPSMain {
     public static void main(String[] args) {playBPS();}
 
     public static void playBPS(){
-//        ActiveAccount.numPlayers(2);
+        ActiveAccount.numPlayers(2);
 
-        Account p1 = new Account ("1", "1", "1", 200);
-        Account p2 = new Account ("2", "2", "2", 200);
-        Account p3 = new Account ("3", "3", "3", 200);
-        ActiveAccount.activeAccounts.clear();
-        ActiveAccount.addActiveAccount(p1);
-        ActiveAccount.addActiveAccount(p2);
+//        Account p1 = new Account ("1", "1", "1", 200);
+//        Account p2 = new Account ("2", "2", "2", 200);
+//        Account p3 = new Account ("3", "3", "3", 200);
+//        ActiveAccount.activeAccounts.clear();
+//        ActiveAccount.addActiveAccount(p1);
+//        ActiveAccount.addActiveAccount(p2);
 
         BPSEngine.intro(ActiveAccount.activeAccounts.size());
 
