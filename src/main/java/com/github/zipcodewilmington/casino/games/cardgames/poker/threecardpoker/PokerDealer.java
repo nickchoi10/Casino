5340af3225cebe012446fbe4e36c87d678d6ce03
Jiayong <jiagains@gmail.com>
2022-07-15 23:52:09 -0400

    add PokerMain and PokerDealer stubs

---
+++ src/main/java/com/github/zipcodewilmington/casino/games/cardgames/poker/threecardpoker/PokerDealer.java
@@ -0,0 +1,9 @@
+package com.github.zipcodewilmington.casino.games.cardgames.poker.threecardpoker;
+
+import com.github.zipcodewilmington.casino.games.cardgames.PlayingCard;
+import com.github.zipcodewilmington.casino.games.cardgames.StandardDeck;
+
+public class PokerDealer {
+    private PokerHand hand;
+    private StandardDeck deck;
+}
---
+++ src/main/java/com/github/zipcodewilmington/casino/games/cardgames/poker/threecardpoker/PokerMain.java
@@ -0,0 +1,24 @@
+package com.github.zipcodewilmington.casino.games.cardgames.poker.threecardpoker;
+
+import com.github.zipcodewilmington.casino.GameInterface;
+import com.github.zipcodewilmington.casino.PlayerInterface;
+
+import java.util.List;
+
+public class PokerMain implements GameInterface {
+    private List<PokerPlayer> players;
+    private PokerDealer dealer;
+    private ThreeCardPokerEngine engine;
+
+    public void add(PlayerInterface player) {
+
+    }
+
+    public void remove(PlayerInterface player) {
+
+    }
+
+    public void run() {}
+
+
+}
