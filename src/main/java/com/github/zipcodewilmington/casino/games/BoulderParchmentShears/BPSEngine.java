6d8ca2514da7bfd7e719dcb5a5f94fca2a6895df
kylehwelch <welch@udel.edu>
2022-07-15 23:20:41 -0400

    BPS 1 player methods working

--- src/main/java/com/github/zipcodewilmington/casino/games/BoulderParchmentShears/BPSEngine.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/BoulderParchmentShears/BPSEngine.java
@@ -1,6 +1,8 @@
 package com.github.zipcodewilmington.casino.games.BoulderParchmentShears;
 
 import com.github.zipcodewilmington.Casino;
+import com.github.zipcodewilmington.casino.Account;
+import com.github.zipcodewilmington.casino.ActiveAccount;
 import com.github.zipcodewilmington.utils.TheScanner;
 
 import java.util.Random;
@@ -8,48 +10,119 @@ import java.util.Scanner;
 
 public class BPSEngine {
     Scanner scan = new Scanner(System.in);
-    Random randy = new Random();
-    protected static final String boulder = "boulder";
-    protected static final String parchment = "parchment";
-    protected static final String shears = "shears";
-    Integer botRoll = 0;
-    String botThrow;
+    static Random randy = new Random();
+    static Integer botRoll = 0;
+    static String botThrow;
 
     public static void intro(int numPlayers) {
         int choice;
-        System.out.println("Welcome to Boulder Parchment Shears. The game of mental dominance over your foes.");
-        if (numPlayers < 3) {
+        System.out.println("\nWelcome to Boulder Parchment Shears. The game of mental dominance over your foes.\n");
+        if (numPlayers == 1) {
+            System.out.println("Would you like to play against an AI, or in a 3-Way match with the AI?\n" +
+                    "1) Player vs. AI\n" +
+                    "2) Player vs. AI vs. AI\n" +
+                    "3) Return to Main Menu\n");
+            choice = TheScanner.getNumber("");
+            if (choice == 1) {
+                PvE();
+            } else if (choice == 2) {
+                PvEvE();
+            } else if (choice == 3) {
+                Casino.splashScreen();
+            } else System.out.println("That is not a valid menu choice, enter the number for your choice.");
+        }
+        if (numPlayers == 2) {
             System.out.println("Would you like to play against each other, or in a 3-Way match with an AI?\n" +
                     "1) Player vs. Player\n" +
                     "2) Player vs. Player vs. AI\n" +
-                    "3) Return to Main Menu");
+                    "3) Return to Main Menu\n");
             choice = TheScanner.getNumber("");
             if (choice == 1) {
-                //make bets and then get choices
+                //launch PvP
             } else if (choice == 2) {
-                //make bets and then get choices
+                //launch PvPvE
             } else if (choice == 3) {
                 Casino.splashScreen();
             } else System.out.println("That is not a valid menu choice, enter the number for your choice.");
         }
         if (numPlayers == 3) {
-            //make bets and then get choices
+            System.out.println("Would you like to play against each other, or in a 3-Way match with an AI?\n" +
+                    "1) Player vs. Player vs. Player\n" +
+                    "2) Return to Main Menu\n");
+            choice = TheScanner.getNumber("");
+            if (choice == 1) {
+                //launch PvPvP
+            } else if (choice == 2) {
+                Casino.splashScreen();
+            } else System.out.println("That is not a valid menu choice, enter the number for your choice.");
         }
     }
-
     public static void BPSRules2P() {
         System.out.println("The rules are simple. Boulder beats Shears, Shears beats Parchment, Parchment beats Boulder.\n" +
-                "You will make a wager against the other player, winner gets all. This means that if you win, you double your bet.\n");
+                "You will make a wager against the other player, winner gets all. All wagers must be equal\n" +
+                "This means that if you win, you double your bet.\n\n");
     }
-
     public static void BPSRules3P() {
         System.out.println("Three player boulder parchment shears is an odd-man-IN type of game.\n" +
-                "If two people throw the same object, the odd man out WINS!\n" +
-                "If all three players throw the same object, it is a draw and you go again. Winner collects all bets.");
+                "If two people throw the same object, the odd man out WINS! If all three players throw the same object, it is a draw and you go again.\n" +
+                "All bets must be equal. Winner collects all bets.\n\n");
+    }
+    public static void PvE() {
+        int wager;
+        String win;
+        String p1;
+        BPSRules2P();
+        System.out.println("How much do you want to wager?\n");
+        wager = Account.makeBet(ActiveAccount.activeAccounts.get(0));
+        p1 = getThrow(1);
+        win = BPS1P(p1);
+        if (p1.equals(win)) {
+            System.out.println(ActiveAccount.activeAccounts.get(0).getAccountName() + "Wins! \n" +
+                    ActiveAccount.activeAccounts.get(0).getAccountName() + " threw " + p1 + " and the AI threw " + getLosingMove(p1) + ".\n" +
+                    "You wagered " + wager + " and won " + (wager*2) + ". That amount has been deposited in your account.\n");
+            Account.deposit(ActiveAccount.activeAccounts.get(0), (wager*2));
+        } else System.out.println(ActiveAccount.activeAccounts.get(0).getAccountName() + " Lost.  :( ");
+        quitPvE();
+    }
+    public static void quitPvE(){
+        int choice;
+        choice = TheScanner.getNumber("1) Play Again\n2) Return to Main Menu\n");
+        if (choice == 1) {PvE();}
+        else Casino.splashScreen();
+    }
+    public static void PvEvE() {
+        int wager;
+        String win;
+        String p1;
+        BPSRules3P();
+        System.out.println("How much do you want to wager?\n");
+        wager = Account.makeBet(ActiveAccount.activeAccounts.get(0));
+        p1 = getThrow(1);
+        String b1 = AIThrow();
+        String b2 = AIThrow();
+        win = getWinner3P(p1, b1, b2);
+        if (p1.equals(win)) {
+            System.out.println(ActiveAccount.activeAccounts.get(0).getAccountName() + " WINS! \n" +
+                    ActiveAccount.activeAccounts.get(0).getAccountName() + " threw " + p1 + " and the AI threw " + getLosingMove(p1) + ".\n" +
+                    "You wagered " + wager + " and won " + (wager * 3) + ". That amount has been deposited in your account.\n");
+            Account.deposit(ActiveAccount.activeAccounts.get(0), (wager * 3));
+        }else if (win.equals("draw")){
+            System.out.println("It's a draw! Wagers have been re-deposited. Play again!\n");
+            Account.deposit(ActiveAccount.activeAccounts.get(0), (wager));
+            PvEvE();
+        }else System.out.println(ActiveAccount.activeAccounts.get(0).getAccountName() + " threw " + p1 + " and the AI's threw " +
+                b1 + " and " + b2 + ". " + ActiveAccount.activeAccounts.get(0).getAccountName() + " lost :(");
+        quitPvEvE();
+    }
+    public static void quitPvEvE(){
+        int choice;
+        choice = TheScanner.getNumber("1) Play Again\n2) Return to Main Menu\n");
+        if (choice == 1) {PvEvE();}
+        else Casino.splashScreen();
     }
 
     public static String getThrow(int player){
-        String p1;
+        String pt;
         System.out.println("Player " + player + ", pick your sign.\n" +
                 "1) Boulder\n" +
                 "2) Parchment\n" +
@@ -57,35 +130,38 @@ public class BPSEngine {
         while (true) {
             int choice = TheScanner.getNumber("");
             if (choice == 1) {
-                p1 = "boulder";
+                pt = "boulder";
                 System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
                 break;
             } else if (choice == 2) {
-                p1 = "parchment";
+                pt = "parchment";
                 System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
                 break;
             } else if (choice == 3) {
-                p1 = "shears";
+                pt = "shears";
                 System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
                 break;
             } else System.out.println("That is not a valid choice, choose 1, 2, or 3.");
         }
-        return p1;
+        return pt;
+    }
+
+    public static boolean winCondition(String winner, String player) {
+        if (player == winner) return true;
+        else return false;
     }
 
 
-
-
-    public String BPSAI (){
+    public static String AIThrow(){
         botRoll = (randy.nextInt(3)+1);
         if (botRoll == 1) {botThrow = "boulder";}
         else if (botRoll == 2) {botThrow = "parchment";}
         else botThrow = "shears";
         return botThrow;
     }
-    public void BPS1P(String player1){
-        String bot = BPSAI();
-        getWinner2P(player1, bot);
+    public static String BPS1P(String player1){
+        String bot = AIThrow();
+        return getWinner2P(player1, bot);
     }
     public String getWinningMove(String handSign) {
         if (handSign.equals("boulder")) {
@@ -94,14 +170,14 @@ public class BPSEngine {
             return "shears";
         } else return "boulder";
     }
-    public String getLosingMove(String handSign) {
+    public static String getLosingMove(String handSign) {
         if (handSign.equals("boulder")) {
             return "shears";
         } else if (handSign.equals("parchment")) {
             return "boulder";
         } else return "parchment";
     }
-    public String getWinner2P(String player1, String player2) {
+    public static String getWinner2P(String player1, String player2) {
         String oneWinner = getLosingMove(player1);
         if (player2.equals(oneWinner)) {
             return player1;
@@ -115,5 +191,4 @@ public class BPSEngine {
         else if (player3.equals(player1)) {return player2;}
         else return draw;
     }
-
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/BoulderParchmentShears/BPSMain.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/BoulderParchmentShears/BPSMain.java
@@ -13,13 +13,16 @@ public class BPSMain {
 
     public static void playBPS(){
 //        ActiveAccount.numPlayers(3);
-        List<Account> activeAccounts = new ArrayList<>();
+
         Account p1 = new Account ("1", "1", "1", 200);
         Account p2 = new Account ("2", "2", "2", 200);
         Account p3 = new Account ("3", "3", "3", 200);
-        activeAccounts.add(p1);
+        ActiveAccount.addActiveAccount(p1);
 
-        BPSEngine.intro(activeAccounts.size());
+        BPSEngine.intro(1);
+
+        //play PvE
+
 
 
     }
