2b3908d069b176a1573bf3218d9633eace60592a
kylehwelch <welch@udel.edu>
2022-07-16 22:42:43 -0400

    made everything more convenient

--- src/main/java/com/github/zipcodewilmington/casino/games/BoulderParchmentShears/BPSEngine.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/BoulderParchmentShears/BPSEngine.java
@@ -1,20 +1,24 @@
 package com.github.zipcodewilmington.casino.games.BoulderParchmentShears;
-
 import com.github.zipcodewilmington.Casino;
 import com.github.zipcodewilmington.casino.Account;
 import com.github.zipcodewilmington.casino.ActiveAccount;
 import com.github.zipcodewilmington.utils.TheScanner;
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class BPSEngine {
+    private Casino casino;
+    private ActiveAccount aa;
+    private Account acct;
     Scanner scan = new Scanner(System.in);
     static Random randy = new Random();
     static Integer botRoll = 0;
     static String botThrow;
 
-    public static void intro(int numPlayers){
+    public void intro(int numPlayers){
+        aa = new ActiveAccount();
+        acct = new Account();
+        casino = new Casino();
         int choice;
         System.out.println("\nWelcome to Boulder Parchment Shears!\nThe game of mental dominance over your foes.\n");
         if (numPlayers == 1) {
@@ -24,46 +28,46 @@ public class BPSEngine {
             PvP();
         }
     }
-    public static void BPSRules2P() {
+    public void BPSRules2P() {
         System.out.println("The rules are simple. Boulder beats Shears, Shears beats Parchment, Parchment beats Boulder.\n" +
                 "Each player will make the same wager, winner gets all.\n\n");
     }
 
     //1-PLAYER GAME STUFF
 
-    public static void PvE() {
+    public void PvE() {
         int wager;
         String p1;
         String p2;
         BPSRules2P();
         System.out.println("How much do you want to wager?\n");
-        wager = Account.makeBet(ActiveAccount.activeAccounts.get(0));
+        wager = acct.makeBet(aa.activeAccounts.get(0));
         p1 = getThrow(1);
         p2 = AIThrow();
         if (pveWin(p1, p2, wager) == true)  {
-            Account.deposit(ActiveAccount.activeAccounts.get(0), (wager*2));
+            acct.deposit(aa.activeAccounts.get(0), (wager*2));
         } else if (draw(p1, p2)) {
-            Account.deposit(ActiveAccount.activeAccounts.get(0), (wager));
+            acct.deposit(aa.activeAccounts.get(0), (wager));
             PvE();
-        } else System.out.println(ActiveAccount.activeAccounts.get(0).getAccountName() + " threw " + p1 + " and the AI threw " + p2 + "." +
-                ActiveAccount.activeAccounts.get(0).getAccountName() + " lost :(\n\n");
+        } else System.out.println(aa.activeAccounts.get(0).getAccountName() + " threw " + p1 + " and the AI threw " + p2 + "." +
+                aa.activeAccounts.get(0).getAccountName() + " lost :(\n\n");
         quitPvE();
     }
-    public static void quitPvE(){
+    public void quitPvE(){
         int choice;
         choice = TheScanner.getNumber("1) Play Again\n2) Return to Main Menu\n");
         if (choice == 1) {PvE();}
-        else Casino.splashScreen();
+        else casino.splashScreen();
     }
-    public static boolean pveWin(String p1, String p2, int wager){
+    public boolean pveWin(String p1, String p2, int wager){
         if (p1.equals(getWinner2P(p1, p2))) {
-            System.out.println(ActiveAccount.activeAccounts.get(0).getAccountName() + "Wins! \n" +
-                    ActiveAccount.activeAccounts.get(0).getAccountName() + " threw " + p1 + " and the AI threw " + p2 + ".\n" +
+            System.out.println(aa.activeAccounts.get(0).getAccountName() + "Wins! \n" +
+                    aa.activeAccounts.get(0).getAccountName() + " threw " + p1 + " and the AI threw " + p2 + ".\n" +
                     "You wagered " + wager + " and won " + (wager*2) + ". That amount has been deposited in your account.\n");
             return true;}
         else return false;
     }
-    public static boolean draw(String p1, String p2) {
+    public boolean draw(String p1, String p2) {
         if (p1.equals(p2)) {
             System.out.println("It's a draw! Wager's have been returned, play again.");
             return true;}
@@ -71,15 +75,15 @@ public class BPSEngine {
     }
 
     //2-PLAYER GAME STUFF
-    public static void PvP() {
+    public void PvP() {
         int wager;
         String win;
         String p1;
         String p2;
         BPSRules2P();
-        System.out.println(ActiveAccount.activeAccounts.get(0).getAccountName() + ", how much do the players want to wager?\n");
-        wager = Account.makeBet(ActiveAccount.activeAccounts.get(0));
-        Account.withdraw(ActiveAccount.activeAccounts.get(1), wager);
+        System.out.println(aa.activeAccounts.get(0).getAccountName() + ", how much do the players want to wager?\n");
+        wager = acct.makeBet(aa.activeAccounts.get(0));
+        acct.withdraw(aa.activeAccounts.get(1), wager);
         p1 = getThrow(1);
         p2 = getThrow(1);
         win = getWinner2P(p1, p2);
@@ -88,24 +92,24 @@ public class BPSEngine {
         } else pvpWin(p1, p2, wager);
         quitPvP();
     }
-    public static void quitPvP(){
+    public void quitPvP(){
         int choice;
         choice = TheScanner.getNumber("1) Play Again\n2) Return to Main Menu\n");
         if (choice == 1) {PvP();}
-        else Casino.splashScreen();
+        else casino.splashScreen();
     }
-    public static boolean pvpWin(String p1, String p2, int wager){
+    public boolean pvpWin(String p1, String p2, int wager){
         if (p1.equals(getWinner2P(p1, p2))) {
-            System.out.println(ActiveAccount.activeAccounts.get(0).getAccountName() + " threw " + p1 + " and " + ActiveAccount.activeAccounts.get(1).getAccountName() + " threw " + p2 +  ".\n\n" +
-                    ActiveAccount.activeAccounts.get(0).getAccountName() + " WINS!!\n" +
+            System.out.println(aa.activeAccounts.get(0).getAccountName() + " threw " + p1 + " and " + aa.activeAccounts.get(1).getAccountName() + " threw " + p2 +  ".\n\n" +
+                    aa.activeAccounts.get(0).getAccountName() + " WINS!!\n" +
                     "You wagered " + wager + " and won " + (wager*2) + ". That amount has been deposited in your account.\n");
-            Account.deposit(ActiveAccount.activeAccounts.get(0), (wager*2));
+            acct.deposit(aa.activeAccounts.get(0), (wager*2));
             return true;
         } else {
-            System.out.println(ActiveAccount.activeAccounts.get(1).getAccountName() + " threw " + p2 + " and " + ActiveAccount.activeAccounts.get(0).getAccountName() + " threw " + p1 +  ".\n\n" +
-                    ActiveAccount.activeAccounts.get(1).getAccountName() + " WINS!!\n" +
+            System.out.println(aa.activeAccounts.get(1).getAccountName() + " threw " + p2 + " and " + aa.activeAccounts.get(0).getAccountName() + " threw " + p1 +  ".\n\n" +
+                    aa.activeAccounts.get(1).getAccountName() + " WINS!!\n" +
                     "You wagered " + wager + " and won " + (wager*2) + ". That amount has been deposited in your account.\n");
-            Account.deposit(ActiveAccount.activeAccounts.get(1), (wager*2));
+            acct.deposit(aa.activeAccounts.get(1), (wager*2));
             return false;}
     }
 
@@ -113,7 +117,7 @@ public class BPSEngine {
 
 
 
-    public static String getThrow(int player){
+    public String getThrow(int player){
         String pt;
         System.out.println("Player " + player + ", pick your sign.\n" +
                 "1) Boulder\n" +
@@ -137,35 +141,35 @@ public class BPSEngine {
         }
         return pt;
     }
-    public static String AIThrow(){
+    public String AIThrow(){
         botRoll = (randy.nextInt(3)+1);
         if (botRoll == 1) {botThrow = "boulder";}
         else if (botRoll == 2) {botThrow = "parchment";}
         else botThrow = "shears";
         return botThrow;
     }
-    public static String getWinningMove(String handSign) {
+    public String getWinningMove(String handSign) {
         if (handSign.equals("boulder")) {
             return "parchment";
         } else if (handSign.equals("parchment")) {
             return "shears";
         } else return "boulder";
     }
-    public static String getLosingMove(String handSign) {
+    public String getLosingMove(String handSign) {
         if (handSign.equals("boulder")) {
             return "shears";
         } else if (handSign.equals("parchment")) {
             return "boulder";
         } else return "parchment";
     }
-    public static String getDraw2P(String handSign) {
+    public String getDraw2P(String handSign) {
         if (handSign.equals("boulder")) {
             return "shears";
         } else if (handSign.equals("parchment")) {
             return "boulder";
         } else return "parchment";
     }
-    public static String getWinner2P(String player1, String player2) {
+    public String getWinner2P(String player1, String player2) {
         String oneWinner = getLosingMove(player1);
         String oneLoser = getLosingMove(player1);
         if (player2.equals(oneWinner)) {return player1;}
@@ -173,7 +177,7 @@ public class BPSEngine {
         else {return "draw";}
     }
 
-    public static void asciiArt(){
+    public void asciiArt(){
         System.out.println("""
                                 
                     _______
